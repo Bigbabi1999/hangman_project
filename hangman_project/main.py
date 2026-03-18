64eca08169cbb2fd2ee975bef051daf410ce3b63
Ali Moos <moosali446@gmail.com>
2026-03-18 13:51:49 +0400

    m

--- hangman_project/main.py
+++ hangman_project/main.py
@@ -1,3 +1,10 @@
 # Game loop
 
-print(str.isalpha())
+import main
+
+def get_main_world():
+    with open("wordlist.txt", "r") as file:
+        words = file.readlines()
+
+    word = main.choice(words).strip()
+    return word
