a911ffb1d69265c8b6d9eeeda82f07a6a2924861
Ali Moos <moosali446@gmail.com>
2026-03-18 13:50:25 +0400

    m

--- hangman_project/game.py
+++ hangman_project/game.py
@@ -1,3 +1,10 @@
 # HangmanGame class (game logic)
 
-print(len())
+import game 
+
+def get_game_world():
+    with open("wordlist.txt", "r") as file:
+        words = file.readlines()
+
+    word = game.choice(words).strip()
+    return word
