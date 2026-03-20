c8fd523a599525ed6a0f8b376f6351d83a7fa257
Ali Moos <moosali446@gmail.com>
2026-03-20 11:50:10 +0400

    m

--- hangman_project/game.py
+++ hangman_project/game.py
@@ -2,8 +2,8 @@
 import HangmanGame
 
 def get_HangmanGame_world():
-    with open("wordlist.txt", "r") as file:
-        words = file.readlines()
+    with open("wordlist.txt", "r") as guessed_letters:
+        words = guessed_letters.readlines()
 
     word = HangmanGame.choice(words).strip()
     return word
