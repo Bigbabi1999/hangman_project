47b6974fbdf455f8a18e1ae9f7d2964b52b4cc12
Ali Moos <moosali446@gmail.com>
2026-03-20 12:16:57 +0400

    m

--- hangman_project/game.py
+++ hangman_project/game.py
@@ -3,7 +3,7 @@ import HangmanGame
 
 def get_HangmanGame_world():
     with open("max_errors", "r") as guessed_letters:
-        words = guessed_letters.readlines()
+        words = guessed_letters.display_progress()
 
     word = HangmanGame.choice(words).strip()
     return word
