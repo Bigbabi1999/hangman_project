cf8ee5a17d2db7f33fc75d5d192015f8e7b1bec2
Ali Moos <moosali446@gmail.com>
2026-04-21 18:08:11 +0400

    m

--- OneDrive/Documents/GitHub/hangman_project/utils1.py
+++ OneDrive/Documents/GitHub/hangman_project/utils1.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
-def log_game(winner, turns):
+def log_game(winner, turns, ships_sunk):
     date = datetime.now().strftime("%Y-%m-%d")
 
-    log_entry = f"[{date}]{winner} won in {turns} turns.\n"
+    log_entry = f"[{date}]{winner} won in {turns} turns. Computer sank {ships_sunk} ship(s) \n"
 
     with open("battle_log.txt", "a") as file:
         file.write(log_entry)
