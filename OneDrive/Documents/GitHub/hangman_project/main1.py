74811bca4e48eaaf9b6f1f2e5f89cafc2e4943e7
Ali Moos <moosali446@gmail.com>
2026-04-23 09:08:03 +0400

    m

--- OneDrive/Documents/GitHub/hangman_project/main1.py
+++ OneDrive/Documents/GitHub/hangman_project/main1.py
@@ -70,7 +70,7 @@ def main():
 
         choice = input("Enter your choice:")
         if choice == "1":
-            play_game
+            play_game()
 
         elif choice == "2":
             view_history()
@@ -83,4 +83,4 @@ def main():
             print("invalid choice, try again")
 
 if __name__ == "__main__":
-    main
+    main()
